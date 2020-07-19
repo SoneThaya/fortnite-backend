6df394f1e4494ddce644cce729f4134be238c1df
Sone <sthaya@gmail.com>
2020-07-19 15:46:36 -0700

    adjusted env file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
 
-dotenv.config({ path: './config.env'})
+dotenv.config({ path: '.env'})
 
 const app = express();
 
