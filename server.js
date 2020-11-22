7849949b75aefc9f2f5c183784347dec634a17af
Sone <sthaya@gmail.com>
2020-11-22 13:54:58 -0800

    use cors

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const morgan = require('morgan')
-const dotenv = require('dotenv')
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-dotenv.config({ path: '.env'})
+dotenv.config({ path: ".env" });
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/store', require('./routes/store'))
+app.use("/api/store", require("./routes/store"));
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`Server listening in ${process.env.NODE_ENV} mode on port ${port}`)
-})
+  console.log(
+    `Server listening in ${process.env.NODE_ENV} mode on port ${port}`
+  );
+});
